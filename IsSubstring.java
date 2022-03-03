0e782b9382c4d9b2e32816a1387e1865300166a6
parmeetnarang2812 <parmeetnarang28@gmail.com>
2022-03-03 18:34:13 +0530

    -

---
+++ IsSubstring.java
@@ -0,0 +1,32 @@
+/*                      392. Is Subsequence - Easy
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+Constraints:
+0 <= s.length <= 100
+0 <= t.length <= 104 
+*/
+
+/*  my leetcode soln
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+        }
+        return j == s.length();
+    }
+} 
+*/
