9e3cd57a14657882c10f8593b4c768cfa68f378b
parmeetnarang2812 <parmeetnarang28@gmail.com>
2022-05-01 19:08:51 +0530

    -

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -26,54 +26,54 @@ Constraints:
 /*  
 my leetcode solns 
 class Solution {
-    //Part1 - memoization
-    // public int climbStairs_mem(int n) {
-    //     int[] dp = new int[n+1];
-    //     Arrays.fill(dp, -1);
-        
-    //     return climb_mem(dp, n);
-    // }
+    public int climbStairs(int n) {
+        // int[] dp = new int[n+1];
+
+        // return climbStairs_recursion(n);
+        // return climbStairs_mem(n, dp);
+        // return climbStairs_tab(n, dp);
+        return climbStairs_optimization(n);
+    }
+
+    public int climbStairs_optimization(int N) {
+        int a=1, b=1;
+        for(int n=0; n<N; n++) {
+            int sum = a+b;
+            a=b;
+            b=sum;
+        }
+        return a;
+    }
     
-    // public static int climb_mem(int[] dp, int n) {
-    //     if(n<0) {
-    //         return 0;
-    //     }
-    //     else if(n==0 || n==1) {
-    //         return 1;
-    //     }
+    public int climbStairs_tab(int N, int[] dp) {
+        for(int n=0; n<=N; n++) {
+            if (n <= 1) {
+                dp[n] = 1;
+                continue;
+            }
+            dp[n] = dp[n-1] + dp[n-2];
+        }
+        return dp[N];
+    }
+    
+    public int climbStairs_mem(int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = 1;
+        }
 
-    //     if(dp[n]!=-1) {
-    //         return dp[n];
-    //     }
+        if(dp[n]!=0) {
+            return dp[n];
+        }
 
-    //     return dp[n] = climb_mem(dp, n-1) + climb_mem(dp, n-2);
+        return dp[n] = climbStairs_mem(n-1, dp) + climbStairs_mem(n-2, dp);
+    }
+    
+    public int climbStairs_recursion(int n) {
+        if (n <= 1) {
+            return 1;
+        }
 
-    // }
-
-    //Part2 - tabulation
-    // public int climbStairs(int n) {
-    //     return climb_tab(n);
-    // }
-    // public int climb_tab(int n) {
-    //     int[] dp = new int[n+1];
-    //     dp[0]=1;
-    //     dp[1]=1;
-    //     for(int i=2; i<=n; i++) {
-    //         dp[i] = dp[i-1] + dp[i-2];
-    //     }
-    //     return dp[n];
-    // }
-
-    //Part3 - without extra space
-    // public int climbStairs(int n) {
-    //     int prev2 = 1;
-    //     int prev = 1;
-    //     for(int i=2; i<=n; i++) {
-    //         int curr = prev2 + prev;
-    //         prev2 = prev;
-    //         prev = curr;
-    //     }
-    //     return prev;
-    // }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
 } 
 */
