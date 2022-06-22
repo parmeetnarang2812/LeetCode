1a1a034e04cfdeaca2e6831c711536e83c2d3a11
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-06-23 01:10:09 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int[] nums = new int[2];
+        for(int i=0; i<n; i++) {
+            int val = Math.abs(arr[i])-1;
+            if(arr[val]<0) {
+                nums[0] = val+1;
+            }
+            else {
+                arr[val] *= -1;
+            }
+        }
+        
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i]>0) {
+                nums[1] = i+1;
+            }
+        }
+        
+        return nums;
+    }
+}
